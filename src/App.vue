f066a6f977fb3fb043090c59909b482df1c3d3c0
stephen.brooks220413 <stephen.brooks220413@gmail.com>
2021-02-07 06:26:54 -0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,105 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <!-- Container wrapper -->
+      <div class="container-fluid">
+        <!-- Navbar brand -->
+        <a class="navbar-brand" href="#">Developing4Life</a>
+
+        <!-- Toggle button -->
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-mdb-toggle="collapse"
+          data-mdb-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i class="fas fa-bars"></i>
+        </button>
+
+        <!-- Collapsible wrapper -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <!-- Left links -->
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about">
+                About
+              </router-link>
+            </li>
+            <!-- Navbar dropdown -->
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-mdb-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <!-- Dropdown menu -->
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <router-link class="dropdown-item" to="/">
+                    Services
+                  </router-link>
+                </li>
+                <li>
+                  <router-link class="dropdown-item" to="/">
+                    Services
+                  </router-link>
+                </li>
+                <li><hr class="dropdown-divider" /></li>
+                <li>
+                  <router-link class="dropdown-item" to="/">
+                    Services
+                  </router-link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <router-link class="dropdown-item" to="/">
+                Gallery
+              </router-link>
+            </li>
+          </ul>
+          <!-- Left links -->
+
+          <!-- Search form -->
+          <form class="d-flex input-group w-auto">
+            <input
+              type="search"
+              class="form-control"
+              placeholder="Type query"
+              aria-label="Search"
+            />
+            <button
+              class="btn btn-outline-primary"
+              type="button"
+              data-mdb-ripple-color="dark"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+        <!-- Collapsible wrapper -->
+      </div>
+      <!-- Container wrapper -->
+    </nav>
+    <br />
+    <router-view />
   </div>
-  <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "~mdb-ui-kit/css/mdb.min.css";
 
 #nav {
   padding: 30px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,87 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <main class="jumbotron img-fluid bg-image text-center">
+      <div class="mask" style="background-color: rgba(0, 0, 0, 0.6)">
+        <div class="d-flex justify-content-center align-items-center h-100">
+          <h1 class="display-4 text-white mb-0">Vue.js Developer</h1>
+        </div>
+      </div>
+    </main>
+    <br />
+    <br />
+    <br />
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card hover-shadow text-center">
+            <div class="card-header">
+              <i class="fab icon fa-connectdevelop"></i>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title">Build Websites</h4>
+              <p class="card-text">
+                Using Vue.js with TypeScript and MDB5 for laying out content and
+                images. Using Vue makes faster load times and very light weight
+                on web browsers.
+              </p>
+              <a
+                href="mailto:stephenbrooks714@gmail.com"
+                class="btn btn-primary"
+                >Inquire</a
+              >
+            </div>
+            <div class="card-footer text-muted">227 Projects</div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card hover-shadow text-center">
+            <div class="card-header">
+              <i class="fas icon fa-globe"></i>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title">Hosting</h4>
+              <p class="card-text">
+                Using Vue.js with TypeScript and MDB5 for laying out content and
+                images. Using Vue makes faster load times and very light weight
+                on web browsers.
+              </p>
+              <a
+                href="mailto:stephenbrooks714@gmail.com"
+                class="btn btn-primary"
+                >Inquire</a
+              >
+            </div>
+            <div class="card-footer text-muted">87 Active Clients</div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card hover-shadow text-center">
+            <div class="card-header">
+              <i class="fas icon fa-tools"></i>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title">Maintenance</h4>
+              <p class="card-text">
+                Using Vue.js with TypeScript and MDB5 for laying out content and
+                images. Using Vue makes faster load times and very light weight
+                on web browsers.
+              </p>
+              <a
+                href="mailto:stephenbrooks714@gmail.com"
+                class="btn btn-primary"
+                >Inquire</a
+              >
+            </div>
+            <div class="card-footer text-muted">
+              Maintaining with TypeScript
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br />
+    <br />
+    <HelloWorld msg="Greetings From Your Vue.js + TypeScript Web Developer" />
   </div>
 </template>
 
@@ -16,3 +96,26 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 })
 export default class Home extends Vue {}
 </script>
+
+<style>
+.home {
+  color: teal;
+}
+main {
+  background: url(https://images.unsplash.com/photo-1496171367470-9ed9a91ea931?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80)
+    no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  height: auto;
+  padding-top: 30%;
+  padding-bottom: 30%;
+  width: 100%;
+}
+.icon {
+  font-size: 2.7em !important;
+  color: grey;
+  opacity: 0.7;
+}
+</style>
